d5b3dfe3c963f38b668f3d76b58ef5e0c4cec3df
Tanya Chandnani <65025878+TanyaChandnani2601@users.noreply.github.com>
2022-01-06 18:37:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-const btn = document.getElementById('btn');
-const color = document.querySelector('.container');
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".container");
 
-btn.addEventListener("click", function(){
-    let hexColor = '#';
-    for(let i = 0; i < 6; i++)
-    {
-        hexColor += hex[getRandomNo()];
-    }
+btn.addEventListener("click", function () {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNo()];
+  }
 
-color.textContent = hexColor;
-document.body.style.backgroundColor = hexColor;
+  color.textContent = hexColor;
+  //   btn.style.color = hexColor + "ff";
+  //   console.log(btn.style.color);
+  document.body.style.backgroundColor = hexColor;
+  var css = "#btn:hover{ color: " + hexColor + "}";
+  var style = document.createElement("style");
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+
+  document.getElementsByTagName("head")[0].appendChild(style);
 });
 // document.getElementsByTagName("btn").style.background="blue";
 
-
-function getRandomNo(){
-    return Math.floor(Math.random() * hex.length);
+function getRandomNo() {
+  return Math.floor(Math.random() * hex.length);
 }
